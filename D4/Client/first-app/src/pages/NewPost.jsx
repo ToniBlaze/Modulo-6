80e41623424f5f3ac3c9715818f625aed0ac587e
Alessio <120385286+ToniBlaze@users.noreply.github.com>
2023-06-13 11:38:34 +0000

    apportate modifiche

---
+++ D4/Client/first-app/src/pages/NewPost.jsx
@@ -0,0 +1,70 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
+import { Navigate, useNavigate } from 'react-router-dom'
+
+
+export default function NewPost() {
+    const [obj, setObj] = useState({})
+    const navigate = useNavigate()
+
+       //Torna ad Home
+   const backToHome = () => {
+    Navigate("/");
+    window.scrollTo(0, 0);
+  };
+
+
+    const handlerChange = (e)=>{
+        
+
+        let {name, value} = e.target;
+        setObj({
+            ...obj,
+            [name]:value
+
+        })
+
+    }
+
+    const handleSubmit =()=>{
+        axios.post("http://localhost:3000/posts", obj)
+        .then(response => {navigate("/")})
+    }
+
+
+
+
+    return (
+        <Container>
+              <Button className="px-4 py-2 my-4 btn-secondary" onClick={backToHome}>
+  Back to Home
+</Button>
+            <h1>New Post</h1>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>category</Form.Label>
+                    <Form.Control onChange={handlerChange}   type="text" name="category" placeholder="" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>title</Form.Label>
+                    <Form.Control onChange={handlerChange}  type="text" name="titolo" placeholder="" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>content</Form.Label>
+                    <Form.Control onChange={handlerChange}  type="text" name="content" placeholder="" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>cover</Form.Label>
+                    <Form.Control onChange={handlerChange}   type="text" name="cover" placeholder="" />
+                </Form.Group>
+                <Button onClick={handleSubmit} variant="primary" type="button">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+    )
+}
+
+
+
